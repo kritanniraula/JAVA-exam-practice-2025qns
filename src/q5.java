704947c4d38c4232ab5afd10583660a0320b989b
Kritan Niraula <kritanniraula@gmail.com>
2026-04-13 08:30:06 +0545

    string immutable

---
+++ src/q5.java
@@ -0,0 +1,20 @@
+public class q5 {
+    public static void main(String[] args) {
+        String s1 = "ram";
+        StringBuffer sb = new StringBuffer("sita");
+
+        System.out.println(s1);
+        System.out.println(sb);
+
+       s1.concat("hello");
+       sb.append("HELLO");
+
+       System.out.println(s1);
+       System.out.println(sb);
+      
+
+   
+    
+
+    }
+}
